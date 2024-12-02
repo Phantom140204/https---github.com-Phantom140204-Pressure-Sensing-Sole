7526dec4801f70a97330bd62f24a11203d51d155
Nithin M <nithin.m@hyperhorizon.in>
2024-12-03 02:26:21 +0530

    add socketio for handling multiple clients

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO, emit
 import socket
 import time
 import webbrowser
@@ -11,81 +12,24 @@ hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 port = 8000
 
-sensor_data = [[0, 0, 0], [0, 0, 0]]
-sensor_left_data_available = 0
-sensor_right_data_available = 0
-
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html', ip=(ip + ":" + str(port)))
 
 @app.route('/sensor_data', methods=['POST'])
-def sensordata():
-    global sensor_data, sensor_left_data_available, sensor_right_data_available
-
+def sensor_data():
     data = request.get_json()
-    if (data['device_id'] == 0):
-        sensor_data[0][0] = data['fsr_toe']
-        sensor_data[0][1] = data['fsr_middle']
-        sensor_data[0][2] = data['fsr_heel']
-        sensor_left_data_available = 1
+    data['timestamp'] = int((time.time() - server_time) * 1000)
 
-    elif (data['device_id'] == 1):
-        sensor_data[1][0] = data['fsr_toe']
-        sensor_data[1][1] = data['fsr_middle']
-        sensor_data[1][2] = data['fsr_heel']
-        sensor_right_data_available = 1
+    socketio.emit('update', data)
     
     return jsonify({'status': 'success'})
 
-@app.route('/sensor_data_0', methods=['GET'])
-def sensor_data_0():
-    global sensor_data, sensor_left_data_available
-
-    if (sensor_left_data_available):
-        sensor_left_data_available = 0
-        return jsonify({
-            'timestamp': int((time.time() - server_time) * 1000),
-            'fsr_toe': sensor_data[0][0],
-            'fsr_middle': sensor_data[0][1],
-            'fsr_heel': sensor_data[0][2],
-            'is_valid': 1
-        })
-    else:
-        return jsonify({
-            'timestamp': int((time.time() - server_time) * 1000),
-            'fsr_toe': sensor_data[0][0],
-            'fsr_middle': sensor_data[0][1],
-            'fsr_heel': sensor_data[0][2],
-            'is_valid': 0
-        })
-
-@app.route('/sensor_data_1', methods=['GET'])
-def sensor_data_1():
-    global sensor_data, sensor_right_data_available
-
-    if (sensor_right_data_available):
-        sensor_right_data_available = 0
-        return jsonify({
-            'timestamp': int((time.time() - server_time) * 1000),
-            'fsr_toe': sensor_data[1][0],
-            'fsr_middle': sensor_data[1][1],
-            'fsr_heel': sensor_data[1][2],
-            'is_valid': 1
-        })
-    else:
-        return jsonify({
-            'timestamp': int((time.time() - server_time) * 1000),
-            'fsr_toe': sensor_data[1][0],
-            'fsr_middle': sensor_data[1][1],
-            'fsr_heel': sensor_data[1][2],
-            'is_valid': 0
-        })
-
 def run_flask():
-    app.run(host=ip, port=port)
+    socketio.run(app, host=ip, port=port)
 
 def run_gui():
     root = tk.Tk()
