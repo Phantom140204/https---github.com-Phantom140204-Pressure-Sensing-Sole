cb4b781694fd8cd1c00a368dafcfc21767c2445f
Nithin M <nithin.m@hyperhorizon.in>
2024-12-24 00:43:07 +0530

    add recording feature

--- webapp.py
+++ webapp.py
@@ -6,26 +6,46 @@ import time
 import webbrowser
 import tkinter as tk
 import threading
+import csv
 
 server_time = time.time()
 
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 port = 8000
+recording = False
+csv_file = 'sensor_data.csv'
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='threading')  # using threading async mode
 
+with open(csv_file, mode ='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=['timestamp', 'device_id', 'fsr_toe', 'fsr_middle', 'fsr_heel'])
+    writer.writeheader()
+
 @app.route('/')
 def index():
-    return render_template('index.html', ip=(ip + ":" + str(port)))
+    return render_template('index.html', ip=(ip + ":" + str(port)), record_status=recording)
 
 @app.route('/sensor_data', methods=['POST'])
 def sensor_data():
     data = request.get_json()
     data['timestamp'] = int((time.time() - server_time) * 1000)
     socketio.emit('update', data)
+
+    if recording:
+        with open(csv_file, mode='a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['timestamp', 'device_id', 'fsr_toe', 'fsr_middle', 'fsr_heel'])
+            writer.writerow(data)
+
     return jsonify({'status': 'success'})
+
+@socketio.on('recording_status')
+def handle_recording_toggle(data):
+    global recording
+    recording = data['recording']
+    socketio.emit('recording_ack',{'recording': recording})
+
 
 def run_flask():
     socketio.run(app, host=ip, port=port, allow_unsafe_werkzeug=True)  # Disable reloader when running in thread
